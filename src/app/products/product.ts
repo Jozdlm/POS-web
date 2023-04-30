ec4c950dc9ecb676addc9f0f2e8955ca16f013cd
Jozuan Lopez <jozuanlopez@gmail.com>
2023-04-29 22:09:19 -0600

    Definido el modelo product en products

---
+++ src/app/products/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: number;
+  code: string;
+  product_name: string;
+  min_stock: number;
+  selling_price: number;
+  img_url: string;
+  category: string;
+  active: boolean;
+}
