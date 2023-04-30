5678c3289411e42a5dabffed04afd5a6a8959633
Jozuan Lopez <jozuanlopez@gmail.com>
2023-04-29 21:10:12 -0600

    Generado el componente item-list en products

---
+++ src/app/products/item-list/item-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.scss']
+})
+export class ItemListComponent {
+
+}
