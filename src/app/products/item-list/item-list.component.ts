e518b23345f0aa49b2399556bffefbaf581d8b89
Jozuan Lopez <jozuanlopez@gmail.com>
2023-04-30 21:11:42 -0600

    Se inyecta el servicio y se obtienen los datos, se renderiza en una tabla en el template

--- src/app/products/item-list/item-list.component.ts
+++ src/app/products/item-list/item-list.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductService } from '../product.service';
 
 @Component({
   standalone: true,
   imports: [CommonModule],
   templateUrl: './item-list.component.html',
-  styleUrls: ['./item-list.component.scss']
+  styleUrls: ['./item-list.component.scss'],
 })
 export class ItemListComponent {
+  private _productService = inject(ProductService);
 
+  public products$ = this._productService.getProducts();
 }
