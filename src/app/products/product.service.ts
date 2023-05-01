6729471ff6020f8b725b0826bf2ac17653bcd02f
Jozuan Lopez <jozuanlopez@gmail.com>
2023-04-30 21:09:59 -0600

    Se obtienen los productos de la API y se les da un formato personalizado

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, catchError, map, tap, throwError } from 'rxjs';
+import { Product } from './product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private _http = inject(HttpClient);
 
-  constructor() { }
+  constructor() {}
+
+  public getProducts(): Observable<Product[]> {
+    return this._http
+      .get<Product[]>('https://localhost:7242/api/products')
+      .pipe(
+        map((data: any[]) => data.map(this._fmtProduct))
+      );
+  }
+
+  private _fmtProduct(product: any): Product {
+    return <Product>{
+      ...product,
+      code: product.barcode,
+      category: product.category.name,
+      active: product.active ? 'Activo' : 'Desactivado',
+    };
+  }
 }
