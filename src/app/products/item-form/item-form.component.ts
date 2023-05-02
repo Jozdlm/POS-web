8842f0c2335b96c88a5e21639b642e0aeb2328de
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-01 21:21:11 -0600

    Generado el componente item-form en products

---
+++ src/app/products/item-form/item-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-item-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './item-form.component.html',
+  styleUrls: ['./item-form.component.scss']
+})
+export class ItemFormComponent {
+
+}
