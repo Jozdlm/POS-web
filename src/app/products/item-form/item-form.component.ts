312dcc92a17beae39c7a9d9025d41d45e97f293f
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-02 21:57:24 -0600

    Se ha importado e inyectado el modulo para formularios reactivos

--- src/app/products/item-form/item-form.component.ts
+++ src/app/products/item-form/item-form.component.ts
@@ -1,16 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../product.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-item-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './item-form.component.html',
   styleUrls: ['./item-form.component.scss']
 })
 export class ItemFormComponent {
   private _productService = inject(ProductService);
+  private _formBuilder = inject(FormBuilder);
 
   public categories$ = this._productService.getCategories();
 }
