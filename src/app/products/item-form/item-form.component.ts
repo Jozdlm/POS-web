fd7f1de38df4eeeb5fb4e9203babefab5e6d7fa0
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-03 22:52:48 -0600

    Se envia el nuevo producto al servicio y se obtiene la respuesta de la API

--- src/app/products/item-form/item-form.component.ts
+++ src/app/products/item-form/item-form.component.ts
@@ -8,7 +8,7 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './item-form.component.html',
-  styleUrls: ['./item-form.component.scss']
+  styleUrls: ['./item-form.component.scss'],
 })
 export class ItemFormComponent {
   private _productService = inject(ProductService);
@@ -21,15 +21,26 @@ export class ItemFormComponent {
     product_name: ['', Validators.required],
     category_id: [0, [Validators.required, Validators.min(1)]],
     min_stock: [0, [Validators.required, Validators.min(0)]],
-    selling_price: [0, [Validators.required, Validators.min(0)]]
+    selling_price: [0, [Validators.required, Validators.min(0)]],
   });
 
   public handleSubmit() {
-    if(this.itemForm.valid) {
-      console.log(this.itemForm.value);
+    if (this.itemForm.valid) {
+      const newProduct = {
+        ...this.itemForm.value,
+        category_id: parseInt(`${this.itemForm.value.category_id}`),
+        img_url: '',
+        active: true,
+        barcode: this.itemForm.value.barcode!,
+        product_name: this.itemForm.value.product_name!,
+        min_stock: this.itemForm.value.min_stock!,
+        selling_price: this.itemForm.value.selling_price!,
+      };
 
-      this.itemForm.reset();
-      this.itemForm.patchValue({category_id: 0});
+      this._productService.createProduct(newProduct).subscribe({
+        next: (value) => console.log(value),
+      });
+      this.itemForm.reset({ category_id: 0 });
     }
   }
 }
