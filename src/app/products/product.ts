c15ae3e67edf22ad4c0e948f6b871ce9d29386f3
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-03 22:48:58 -0600

    Definido el DTO del producto que se envia a la API

--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -14,3 +14,13 @@ export interface Category {
   name: string;
   description: string;
 }
+
+export interface ProductDto {
+  barcode: string;
+  product_name: string;
+  min_stock: number;
+  selling_price: number;
+  img_url: string;
+  category_id: number;
+  active: boolean;
+}
