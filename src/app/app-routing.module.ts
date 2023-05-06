ff8d5dbefcc9818dd3c457cc7dbc37c0ae5e8624
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-05 21:31:49 -0600

    Generado componente ItemDetails y se le ha asignado una ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ItemListComponent } from './products/item-list/item-list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ItemDetailsComponent } from './products/item-details/item-details.component';
 
 const routes: Routes = [
   {path: 'products', component: ItemListComponent},
+  {path: 'products/:id', component: ItemDetailsComponent},
   {path: '', component: WelcomeComponent, pathMatch: 'full'}
 ];
 
