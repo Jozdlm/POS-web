c3aa86e8a9703ebde38d5f27c7b7ad9d9e0f45f0
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-05 21:55:04 -0600

    Se obtiene el producto mediante el id utilizando el ProductService

--- src/app/products/item-details/item-details.component.ts
+++ src/app/products/item-details/item-details.component.ts
@@ -1,20 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-item-details',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './item-details.component.html',
-  styleUrls: ['./item-details.component.scss']
+  styleUrls: ['./item-details.component.scss'],
 })
 export class ItemDetailsComponent {
+  private _productService = inject(ProductService);
   public route = inject(ActivatedRoute);
+
   public productId = 0;
+  public product = {};
 
   constructor() {
     this.productId = Number(this.route.snapshot.params['id']);
-    console.log(this.productId);
+    this._productService.getProductById(this.productId).subscribe({
+      next: (value) => {
+        console.log(value);
+        this.product = value;
+      },
+    });
   }
 }
