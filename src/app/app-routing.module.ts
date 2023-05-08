60dd4a03d862846ba748a24709735d3555744af4
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-07 22:19:09 -0600

    Actualizada la ruta inicial de products para renderizar item-shell

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ItemListComponent } from './products/item-list/item-list.component';
+
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ItemDetailsComponent } from './products/item-details/item-details.component';
+import { ItemShellComponent } from './products/item-shell/item-shell.component';
 
 const routes: Routes = [
-  {path: 'products', component: ItemListComponent},
+  {path: 'products', component: ItemShellComponent},
   {path: 'products/:id', component: ItemDetailsComponent},
   {path: '', component: WelcomeComponent, pathMatch: 'full'}
 ];
