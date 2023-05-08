f2f1f22321f4f6af2adc119a1a80dc9a134572b9
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-07 22:44:54 -0600

    El componente smart ahora renderiza los dumb components relacionados a los items

--- src/app/products/item-shell/item-shell.component.ts
+++ src/app/products/item-shell/item-shell.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../product.service';
 import { Product } from '../product';
+import { ItemFormComponent } from '../item-form/item-form.component';
+import { ItemListComponent } from '../item-list/item-list.component';
 
 @Component({
   selector: 'app-item-shell',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ItemFormComponent, ItemListComponent],
   templateUrl: './item-shell.component.html',
   styleUrls: ['./item-shell.component.scss']
 })
