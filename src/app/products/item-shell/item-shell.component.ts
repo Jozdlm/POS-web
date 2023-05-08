a0a1998a44f429ef2094b1113f08ac48a1e18115
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-07 22:17:35 -0600

    Generado el componente smart item-shell para agrupar los componentes de item

---
+++ src/app/products/item-shell/item-shell.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-item-shell',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './item-shell.component.html',
+  styleUrls: ['./item-shell.component.scss']
+})
+export class ItemShellComponent {
+
+}
