0301cb2d62cc76e8f724f565aa26eca0f587a31e
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-09 21:56:47 -0600

    Se ha definido los elementos del formulario reactivo con sus valores y validaciones

--- src/app/products/item-update/item-update.component.ts
+++ src/app/products/item-update/item-update.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-item-update',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './item-update.component.html',
-  styleUrls: ['./item-update.component.scss']
+  styleUrls: ['./item-update.component.scss'],
 })
 export class ItemUpdateComponent {
-
+  private _formBuilder = inject(FormBuilder);
+  public itemForm = this._formBuilder.group({
+    barcode: [''],
+    product_name: ['', Validators.required],
+    category_id: [0, [Validators.required, Validators.min(1)]],
+    min_stock: [0, [Validators.required, Validators.min(0)]],
+    selling_price: [0, [Validators.required, Validators.min(0)]],
+    img_url: [''],
+    active: [1, [Validators.required]]
+  });
 }
