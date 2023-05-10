176fcd6ab2e1085ab7cccac96776a1d3643b91c9
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-09 21:39:39 -0600

    Generado el componente item-update y se le ha asignado una ruta en la app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ItemDetailsComponent } from './products/item-details/item-details.component';
 import { ItemShellComponent } from './products/item-shell/item-shell.component';
+import { ItemUpdateComponent } from './products/item-update/item-update.component';
 
 const routes: Routes = [
   {path: 'products', component: ItemShellComponent},
+  {path: 'products/edit/:id', component: ItemUpdateComponent},
   {path: 'products/:id', component: ItemDetailsComponent},
   {path: '', component: WelcomeComponent, pathMatch: 'full'}
 ];
---
+++ src/app/products/item-update/item-update.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-item-update',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './item-update.component.html',
+  styleUrls: ['./item-update.component.scss']
+})
+export class ItemUpdateComponent {
+
+}
