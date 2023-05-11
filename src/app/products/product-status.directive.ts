39493f219edd1cb857cc093349c00f0c29eefaf4
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-10 22:31:08 -0600

    Generada la directiva personalizada product-status

---
+++ src/app/products/product-status.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[ProductStatus]',
+  standalone: true
+})
+export class ProductStatusDirective {
+
+  constructor() { }
+
+}
