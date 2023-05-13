0a1dc7eb3a67686deb5b11f94ddfbfe4f6c1b79b
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-12 21:20:36 -0600

    Definido el formulario reactivo y vinculado con el template

--- src/app/products/item-shell/item-shell.component.ts
+++ src/app/products/item-shell/item-shell.component.ts
@@ -4,19 +4,25 @@ import { ProductService } from '../product.service';
 import { Category, Product, ProductDto } from '../product';
 import { ItemFormComponent } from '../item-form/item-form.component';
 import { ItemListComponent } from '../item-list/item-list.component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-item-shell',
   standalone: true,
-  imports: [CommonModule, ItemFormComponent, ItemListComponent],
+  imports: [CommonModule, ItemFormComponent, ItemListComponent, ReactiveFormsModule],
   templateUrl: './item-shell.component.html',
   styleUrls: ['./item-shell.component.scss']
 })
 export class ItemShellComponent {
   private _productService = inject(ProductService);
+  private _formBuilder = inject(FormBuilder);
 
   public products: Product[] = [];
   public categories: Category[] = [];
+
+  public searchForm = this._formBuilder.group({
+    query: ['', Validators.required]
+  })
 
   constructor() {
     this._productService.getProducts().subscribe({
