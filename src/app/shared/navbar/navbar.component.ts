ce9d1d7eff9463df484373d6766ab0d37f8a9061
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-20 21:33:15 -0600

    Define los enlaces de navegacion y los renderiza

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -2,13 +2,35 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+interface NavItem {
+  path: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  public navItems: NavItem[] = [
+    {
+      path: '',
+      name: 'Inicio',
+    },
+    {
+      path: '/products',
+      name: 'Productos',
+    },
+    {
+      path: '/categories',
+      name: 'Categorías',
+    },
+    {
+      path: '/quotations',
+      name: 'Cotizaciones',
+    },
+  ];
 }
