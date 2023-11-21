73d409055270e8ae902d7d2f767a037b6b8eace9
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-20 20:28:53 -0600

    Define form-control con un debounceTime para cada cambio

--- src/app/quotations/pages/add-quotation/add-quotation.component.ts
+++ src/app/quotations/pages/add-quotation/add-quotation.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './add-quotation.component.html',
-  styleUrl: './add-quotation.component.scss'
+  styleUrl: './add-quotation.component.scss',
 })
 export class AddQuotationComponent {
+  public searchControl = new FormControl('');
 
+  constructor() {
+    this.searchControl.valueChanges
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe((value) => this.searchProduct(value));
+  }
+
+  public searchProduct(query: string | null): void {}
 }
