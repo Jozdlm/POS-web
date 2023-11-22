7fc71ead7d52cbf3fa9bce8546f511e26cbf72cb
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-21 21:27:32 -0600

    Metodo para añadir un quotation-item y actualizar el estado

--- src/app/quotations/services/quotation-state.service.ts
+++ src/app/quotations/services/quotation-state.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { QuotationItem } from '../models/quotation-item';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuotationStateService {
+  private _items: QuotationItem[] = [];
+  private _stateEmitter = new BehaviorSubject<QuotationItem[]>([]);
+  public readonly items$ = this._stateEmitter.asObservable();
 
   constructor() { }
+
+  public addItem(item: QuotationItem): void {
+    // TODO: Validates if the item is already exist at items array
+    this._items = [...this._items, item];
+    this._stateEmitter.next(this._items);
+  }
 }
