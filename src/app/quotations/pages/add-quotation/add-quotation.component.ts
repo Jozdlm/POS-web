be6324a0330fc625a23df893716ca6c87b75605e
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-21 18:40:52 -0600

    Obtienes los grados escolares del servicio

--- src/app/quotations/pages/add-quotation/add-quotation.component.ts
+++ src/app/quotations/pages/add-quotation/add-quotation.component.ts
@@ -4,6 +4,8 @@ import { RouterModule } from '@angular/router';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 import { SupabaseService } from 'src/app/core/services/supabase.service';
+import { SchoolGradeService } from '../../services/school-grade.service';
+import { SchoolGrade } from '../../models/school-grades';
 
 @Component({
   standalone: true,
@@ -13,10 +15,14 @@ import { SupabaseService } from 'src/app/core/services/supabase.service';
 })
 export class AddQuotationComponent {
   private readonly _supabaseService = inject(SupabaseService);
+  private readonly _schoolGradeService = inject(SchoolGradeService);
   private readonly _supabase = this._supabaseService.supabase;
   public searchControl = new FormControl('');
   // TODO: Add typo to array type
   public filteredProducts: any[] = [];
+  public schoolGrades: SchoolGrade[] = [];
+
+  // TODO: Create a control group for quotations
 
   constructor() {
     this.searchControl.valueChanges
@@ -27,6 +33,16 @@ export class AddQuotationComponent {
           this.filteredProducts = products;
         }
       });
+    this.getSchoolGrades();
+  }
+
+  private async getSchoolGrades(): Promise<void> {
+    try {
+      const grades = await this._schoolGradeService.getSchoolGrades();
+      this.schoolGrades = grades;
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   // TODO: Add typo to return type
