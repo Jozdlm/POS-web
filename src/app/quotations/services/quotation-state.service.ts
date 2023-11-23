81cd7ce271a277628ce76eabb2783c3ff2f4badb
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-22 21:35:11 -0600

    Metodo que elimina un item de la cotización y emite el nuevo state

--- src/app/quotations/services/quotation-state.service.ts
+++ src/app/quotations/services/quotation-state.service.ts
@@ -3,18 +3,23 @@ import { QuotationItem } from '../models/quotation-item';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuotationStateService {
   private _items: QuotationItem[] = [];
   private _stateEmitter = new BehaviorSubject<QuotationItem[]>([]);
   public readonly items$ = this._stateEmitter.asObservable();
 
-  constructor() { }
+  constructor() {}
 
   public addItem(item: QuotationItem): void {
     // TODO: Validates if the item is already exist at items array
     this._items = [...this._items, item];
+    this._stateEmitter.next(this._items);
+  }
+
+  public removeItem(itemId: number): void {
+    this._items = [...this._items.filter((item) => item.productId != itemId)];
     this._stateEmitter.next(this._items);
   }
 }
