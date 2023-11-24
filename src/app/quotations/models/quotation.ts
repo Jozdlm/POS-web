af44075716f56477d886e0ae93142767f84bc60f
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-23 18:52:55 -0600

    Define la entidad quotation

---
+++ src/app/quotations/models/quotation.ts
@@ -0,0 +1,12 @@
+import { QuotationItem } from './quotation-item';
+
+export interface Quotation {
+  id?: number;
+  customerName: string;
+  studentName: string;
+  date: string;
+  schoolGrade: number;
+  schoolName: string;
+  totalAmmount: number;
+  items: QuotationItem[];
+}
