39844d808c65717743311119223ea78f0b83b470
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-23 21:39:24 -0600

    Inserta la metadata de la cotización en la DB y obtiene el id

--- src/app/quotations/services/quotation.service.ts
+++ src/app/quotations/services/quotation.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Quotation, QuotationDto } from '../models/quotation';
+import { SupabaseService } from '@app/core/services/supabase.service';
+import { QuotationItemDto } from '../models/quotation-item';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QuotationService {
+  private readonly _supabaseService = inject(SupabaseService);
+  private readonly _supabase = this._supabaseService.supabase;
+
   constructor() {}
 
   private _mapperQuotation(source: Quotation): QuotationDto {
@@ -18,7 +23,23 @@ export class QuotationService {
     };
   }
 
+  private async _insertHeader(header: QuotationDto): Promise<any> {
+    const { data, error } = await this._supabase
+      .from('quotation_header')
+      .insert(header)
+      .select();
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return data[0];
+  }
+
   public async createQuotation(quotation: Quotation): Promise<void> {
     const quotationDto: QuotationDto = this._mapperQuotation(quotation);
+
+    const insertedHeader = await this._insertHeader(quotationDto);
+    const quotationId: number = insertedHeader.id;
   }
 }
