8a2f833b626286afbf6bc58bb80d4bcefa9d5460
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-23 21:02:35 -0600

    Prepara los datos del quotation para ser insertados en la db

--- src/app/quotations/services/quotation.service.ts
+++ src/app/quotations/services/quotation.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Quotation, QuotationDto } from '../models/quotation';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuotationService {
+  constructor() {}
 
-  constructor() { }
+  public createQuotation(data: Quotation): void {
+    const quotationDto: QuotationDto = {
+      customer_name: data.customerName,
+      student_name: data.studentName,
+      date: data.date,
+      school_grade: data.schoolGrade,
+      school_name: data.schoolName,
+      total_ammount: data.totalAmmount,
+    }
+
+    console.log(quotationDto)
+  }
 }
