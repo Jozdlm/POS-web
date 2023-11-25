65a9be29821a5ad63891630ea092556f6315c76b
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-24 22:03:19 -0600

    Genera el componente quotation-details

---
+++ src/app/quotations/pages/quotation-details/quotation-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-quotation-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './quotation-details.component.html',
+  styleUrl: './quotation-details.component.scss'
+})
+export class QuotationDetailsComponent {
+
+}
