ffba091077863d9c5759b14772967a8d163e8765
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-24 19:49:46 -0600

    Define el formulario reactivo para el login

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent {
+  private readonly _formBuilder = inject(FormBuilder);
 
+  // TODO: Validate if the email is valide with custom validator
+  public loginForm = this._formBuilder.nonNullable.group({
+    email: ['', [Validators.required, Validators.minLength(3)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
 }
