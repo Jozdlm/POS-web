1f2fadf6872f24ccc54123df3af418636f679f69
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-24 21:19:34 -0600

    Inicia sesion con las credenciales del login-form

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,20 +1,38 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SupabaseService } from '@app/core/services/supabase.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent {
+  private readonly _supaService = inject(SupabaseService);
   private readonly _formBuilder = inject(FormBuilder);
+  private readonly _router = inject(Router);
+  private readonly _supabase = this._supaService.supabase;
 
   // TODO: Validate if the email is valide with custom validator
   public loginForm = this._formBuilder.nonNullable.group({
     email: ['', [Validators.required, Validators.minLength(3)]],
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
+
+  // TODO: Create a service that handle all the logic of login
+  public async login() {
+    const credentials = this.loginForm.getRawValue();
+
+    let { error } = await this._supabase.auth.signInWithPassword(credentials);
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    this._router.navigateByUrl('/quotations');
+  }
 }
