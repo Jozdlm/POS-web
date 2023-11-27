8d9294b3e3b6767d1b3fbbbd7cb83aea3f707bbc
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-26 22:25:16 -0600

    Obtiene la informacion de la cotizacion desde el servicio

--- src/app/quotations/pages/quotation-details/quotation-details.component.ts
+++ src/app/quotations/pages/quotation-details/quotation-details.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QuotationService } from '@app/quotations/services/quotation.service';
 
 @Component({
   selector: 'app-quotation-details',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './quotation-details.component.html',
-  styleUrl: './quotation-details.component.scss'
+  styleUrl: './quotation-details.component.scss',
 })
 export class QuotationDetailsComponent {
+  private readonly _quotationService = inject(QuotationService);
 
+  constructor() {
+    this._quotationService
+      .getQuotationById(6)
+      .then((quotation) => console.log(quotation))
+      .catch((err) => console.error(err));
+  }
 }
