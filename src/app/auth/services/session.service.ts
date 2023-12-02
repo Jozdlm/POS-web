20f71ff4869f3e2152d6eaec61db9c916033ce63
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-01 21:22:49 -0600

    Metodo que inicia sesión con las credenciales que recibe utilizando supbase

--- src/app/auth/services/session.service.ts
+++ src/app/auth/services/session.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { SupabaseService } from '@app/core/services/supabase.service';
+import { LoginCredentials } from '../models/login-credentials';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SessionService {
+  private readonly _supaService = inject(SupabaseService);
+  private readonly _router = inject(Router);
+  private readonly _db = this._supaService.supabase;
 
-  constructor() { }
+  constructor() {}
+
+  public async login(credentials: LoginCredentials) {
+    let { error } = await this._db.auth.signInWithPassword(credentials);
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    this._router.navigateByUrl('/quotations');
+  }
 }
