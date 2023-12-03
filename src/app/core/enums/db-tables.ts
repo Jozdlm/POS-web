40fa8246e3c0840d6c18b66ee4ce609c477efd7a
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-03 09:20:39 -0600

    Define enum con los nombres de las tablas de supabase

---
+++ src/app/core/enums/db-tables.ts
@@ -0,0 +1,9 @@
+// These are the table names of supabase database
+export enum DbTables {
+  QUOTATIONS = 'quotation_header',
+  QUOTATION_ITEMS = 'quotation_items',
+  PRODUCTS = 'products',
+  CATEGORIES = 'categories',
+  SCHOOL_GRADES = 'school_grades',
+  SCHOOLS = 'schools',
+}
