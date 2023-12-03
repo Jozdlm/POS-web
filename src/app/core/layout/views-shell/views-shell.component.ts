8670373507903a16362fa075bda97b636398e40d
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-03 09:41:34 -0600

    Genera el componente views-shell para envolver las vistas privadas

---
+++ src/app/core/layout/views-shell/views-shell.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-views-shell',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './views-shell.component.html',
+  styleUrl: './views-shell.component.scss'
+})
+export class ViewsShellComponent {
+
+}
