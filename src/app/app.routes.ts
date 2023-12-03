409c9e013f75544e64ebf6f678e66e5ebc9976a5
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-03 16:47:50 -0600

    Define las rutas hijas del quotation-feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,9 @@
-import { Routes } from "@angular/router";
-import { ItemDetailsComponent } from "./products/item-details/item-details.component";
-import { ItemShellComponent } from "./products/item-shell/item-shell.component";
-import { ItemUpdateComponent } from "./products/item-update/item-update.component";
-import { CategoryListComponent } from "./products/pages/category-list/category-list.component";
-import { WelcomeComponent } from "./welcome/welcome.component";
-import { QuotationComponent } from "./quotations/quotation.component";
-import { AddQuotationComponent } from "./quotations/pages/add-quotation/add-quotation.component";
-import { QuotationDetailsComponent } from "./quotations/pages/quotation-details/quotation-details.component";
+import { Routes } from '@angular/router';
+import { ItemDetailsComponent } from './products/item-details/item-details.component';
+import { ItemShellComponent } from './products/item-shell/item-shell.component';
+import { ItemUpdateComponent } from './products/item-update/item-update.component';
+import { CategoryListComponent } from './products/pages/category-list/category-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 // TODO: Defines guards to prevent an anon client to come in to private pages
 export const APP_ROUTES: Routes = [
@@ -14,8 +11,9 @@ export const APP_ROUTES: Routes = [
   { path: 'products', component: ItemShellComponent },
   { path: 'products/edit/:id', component: ItemUpdateComponent },
   { path: 'products/:id', component: ItemDetailsComponent },
-  { path: 'quotations', component: QuotationComponent },
-  { path: 'quotations/add', component: AddQuotationComponent },
-  { path: 'quotations/:id', component: QuotationDetailsComponent },
+  {
+    path: 'quotations',
+    loadChildren: () => import('./quotations/quotation.routes'),
+  },
   { path: '', component: WelcomeComponent, pathMatch: 'full' },
 ];
