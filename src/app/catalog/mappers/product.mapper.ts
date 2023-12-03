2d80424943480e9eb0feb0f753de3f461f45135c
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-03 10:41:38 -0600

    Define metodo para convertir un dto a un product

--- src/app/catalog/mappers/product.mapper.ts
+++ src/app/catalog/mappers/product.mapper.ts
@@ -1,4 +1,20 @@
+import { Product, ProductDto } from '@app/quotations/models/product';
+
 export class ProductMapper {
-  public static toEntity() {}
+  public static toEntity(dto: ProductDto): Product {
+    if (!dto.id) {
+      throw new Error('The id should provided to map from Dto to Entity');
+    }
+
+    return {
+      id: dto.id,
+      barcode: dto.barcode,
+      name: dto.name,
+      sellingPrice: dto.selling_price,
+      inStock: dto.in_stock,
+      isActive: dto.is_active,
+    };
+  }
+
   public static toDto() {}
 }
