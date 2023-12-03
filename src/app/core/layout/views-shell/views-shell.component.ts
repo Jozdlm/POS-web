d12af41c693a89004a37a0eaf296ea124d02be92
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-03 16:46:00 -0600

    Define el layout básico para el envoltorio de las vistas

--- src/app/core/layout/views-shell/views-shell.component.ts
+++ src/app/core/layout/views-shell/views-shell.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-views-shell',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, NavbarComponent],
   templateUrl: './views-shell.component.html',
-  styleUrl: './views-shell.component.scss'
+  styleUrl: './views-shell.component.scss',
 })
-export class ViewsShellComponent {
-
-}
+export class ViewsShellComponent {}
