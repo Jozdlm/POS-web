99c6de320f9c8660e489fbc07d2139e9e9d6f90b
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-03 21:42:24 -0600

    Crea la vista correspondiente para el login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SessionService } from './services/session.service';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
 })
 export class AuthComponent {
+  private readonly _formBuilder = inject(FormBuilder);
+  private readonly _sessionService = inject(SessionService);
 
+  // TODO: Validate if the email is valide with custom validator
+  public loginForm = this._formBuilder.nonNullable.group({
+    email: ['', [Validators.required, Validators.minLength(3)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  public async login() {
+    const credentials = this.loginForm.getRawValue();
+    // TODO: Create a handler to show an error in the template
+    await this._sessionService.login(credentials);
+  }
 }
