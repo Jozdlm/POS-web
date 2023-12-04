827fd322515c8158566d27e2c59eb8397e182c05
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-03 18:16:41 -0600

    Define estilos básicos para el layout de las vistas privadas

--- src/app/core/layout/views-shell/views-shell.component.ts
+++ src/app/core/layout/views-shell/views-shell.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { SidenavComponent } from '../sidenav/sidenav.component';
 
 @Component({
   selector: 'app-views-shell',
   standalone: true,
-  imports: [CommonModule, RouterModule, NavbarComponent],
+  imports: [CommonModule, RouterModule, NavbarComponent, SidenavComponent],
   templateUrl: './views-shell.component.html',
   styleUrl: './views-shell.component.scss',
 })
