7f81f0815df91dbb127e0c2caf04d937258db5da
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-05 21:05:24 -0600

    Define una ruta vacía para implementar el patrón component-less

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,22 @@ import { ItemShellComponent } from './products/item-shell/item-shell.component';
 import { ItemUpdateComponent } from './products/item-update/item-update.component';
 import { CategoryListComponent } from './products/pages/category-list/category-list.component';
 import { AppComponent } from './app.component';
+import { AddQuotationComponent } from './quotations/pages/add-quotation/add-quotation.component';
 
 // TODO: Defines guards to prevent an anon client to come in to private pages
 export const APP_ROUTES: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.routes') },
-  { path: 'categories', component: CategoryListComponent },
-  { path: 'products', component: ItemShellComponent },
-  { path: 'products/edit/:id', component: ItemUpdateComponent },
-  { path: 'products/:id', component: ItemDetailsComponent },
   {
-    path: 'quotations',
-    loadChildren: () => import('./quotations/quotation.routes'),
+    path: '',
+    children: [
+      {
+        path: 'quotations',
+        loadChildren: () => import('./quotations/quotation.routes'),
+      },
+      { path: 'categories', component: CategoryListComponent },
+      { path: 'products', component: ItemShellComponent },
+      { path: 'products/edit/:id', component: ItemUpdateComponent },
+      { path: 'products/:id', component: ItemDetailsComponent },
+    ],
   },
-  { path: '', component: AppComponent, pathMatch: 'full' },
 ];
