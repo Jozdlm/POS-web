f64f84ec75d291c9612fb1fe4958f069191a42eb
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-09 08:31:08 -0600

    Define enum con los nombres de las rutas principales

---
+++ src/app/core/enums/pages.ts
@@ -0,0 +1,11 @@
+// The enum with the prefixes of the main routes
+export enum PagePrefix {
+  AUTH = 'auth',
+  CATEGORIES = 'categories',
+  GRADES = 'school-grades',
+  HOME = '',
+  PRINT = 'print',
+  PRODUCTS = 'products',
+  QUOTATIONS = 'quotations',
+  SCHOOLS = 'schools',
+}
