184d837a05c4114ce6e05498a8b87a83fb840a76
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-09 11:40:22 -0600

    Define las variables iniciales del entorno para dev y prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,5 @@
-export const environment = {}
+export const environment = {
+  supabaseUrl: 'https://grvggsbcrwpzcjvrfvhf.supabase.co',
+  publicKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdydmdnc2JjcndwemNqdnJmdmhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA4NTU0NTksImV4cCI6MjAwNjQzMTQ1OX0.KXtUaNjr2CIxW7iMmOh-cA4Ys05Y9zgiTkOCBoApiwM',
+};
