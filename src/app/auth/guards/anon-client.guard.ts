d95c2f4b223427b613cecf47935fc2343db9dc88
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-08 18:25:46 -0600

    Implementa lógica para redirigir o mostrar la vista login

--- src/app/auth/guards/anon-client.guard.ts
+++ src/app/auth/guards/anon-client.guard.ts
@@ -1,5 +1,13 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { SessionService } from '../services/session.service';
 
-export const anonClientGuard: CanActivateFn = (route, state) => {
+export const anonClientGuard: CanActivateFn = (_, __) => {
+  const isLogged = inject(SessionService).isClientLogged;
+
+  if (isLogged) {
+    return inject(Router).createUrlTree(['/']);
+  }
+
   return true;
 };
