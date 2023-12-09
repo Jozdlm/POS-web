2dee06eada2060c57b1d86615750b27f746a5425
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-08 18:26:28 -0600

    Utiliza los guards anon-client y logged-client

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,26 @@ import { ItemDetailsComponent } from './products/item-details/item-details.compo
 import { ItemShellComponent } from './products/item-shell/item-shell.component';
 import { ItemUpdateComponent } from './products/item-update/item-update.component';
 import { CategoryListComponent } from './products/pages/category-list/category-list.component';
-import { AppComponent } from './app.component';
 import { AddQuotationComponent } from './quotations/pages/add-quotation/add-quotation.component';
+import { anonClientGuard } from './auth/guards/anon-client.guard';
+import { loggedClientGuard } from './auth/guards/logged-client.guard';
 
 // TODO: Defines guards to prevent an anon client to come in to private pages
 export const APP_ROUTES: Routes = [
-  { path: 'auth', loadChildren: () => import('./auth/auth.routes') },
+  {
+    path: 'auth',
+    canMatch: [anonClientGuard],
+    loadChildren: () => import('./auth/auth.routes'),
+  },
   {
     path: '',
+    canActivate: [loggedClientGuard],
     children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: AddQuotationComponent,
+      },
       {
         path: 'quotations',
         loadChildren: () => import('./quotations/quotation.routes'),
