ecea8609169aa8521083a7ffaaea0cf33c03249b
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-08 18:20:48 -0600

    Implementa la lógica para autorizar o denegar el acceso a las rutas privadas

--- src/app/auth/guards/logged-client.guard.ts
+++ src/app/auth/guards/logged-client.guard.ts
@@ -1,5 +1,13 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { SessionService } from '../services/session.service';
 
-export const loggedClientGuard: CanActivateFn = (route, state) => {
-  return true;
+export const loggedClientGuard: CanActivateFn = (_, __) => {
+  const isLogged = inject(SessionService).isClientLogged;
+
+  if (!isLogged) {
+    return inject(Router).createUrlTree(['/', 'auth']);
+  }
+
+  return isLogged;
 };
