127c3abe9caa818377b5cf46cec4454b01db13c3
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-08 21:38:18 -0600

    Renderiza dinamicamente la navegación del sidenav

--- src/app/core/layout/sidenav/sidenav.component.ts
+++ src/app/core/layout/sidenav/sidenav.component.ts
@@ -1,16 +1,48 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SessionService } from '@app/auth/services/session.service';
+import { NavItem } from '@app/core/interfaces/nav-item';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss'
+  styleUrl: './sidenav.component.scss',
 })
 export class SidenavComponent {
   private readonly _sessionService = inject(SessionService);
+  public readonly navItems: NavItem[] = [
+    {
+      path: '',
+      placeholder: 'Inicio',
+    },
+    {
+      path: '/dashboard',
+      placeholder: 'Dashboard',
+    },
+    {
+      path: '/quotations',
+      placeholder: 'Cotizaciones',
+    },
+    {
+      path: '/products',
+      placeholder: 'Productos',
+    },
+    {
+      path: '/categories',
+      placeholder: 'Categorías',
+    },
+    {
+      path: '/schools',
+      placeholder: 'Centros Educativos',
+    },
+    {
+      path: '/school-grades',
+      placeholder: 'Grados Académicos',
+    },
+  ];
 
   public async handleLogoutEvent(): Promise<void> {
     await this._sessionService.logOut();
