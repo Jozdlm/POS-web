657205383cb5472cc9acebf9075dc96317b4a63a
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-10 15:23:18 -0600

    Muestra los productos en la vista

--- src/app/catalog/pages/product-list/product-list.component.ts
+++ src/app/catalog/pages/product-list/product-list.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductService } from '@app/catalog/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent {
-
+  public readonly products$ = inject(ProductService).getProducts();
 }
