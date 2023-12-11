704270c147facd47594f604a8732b9f9a1f4558b
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-10 20:18:03 -0600

    Mapea una categoría desde un Dto a una Entidad

---
+++ src/app/catalog/mappers/category.mapper.ts
@@ -0,0 +1,18 @@
+import { Category, CategoryDto } from '../models/category';
+
+export class CategoryMapper {
+  public static toEntity(dto: CategoryDto): Category {
+    if (!dto.id) {
+      throw new Error('The Id should be provided to map from Dto to Entity');
+    }
+
+    return {
+      id: dto.id,
+      name: dto.name,
+      description: dto.description,
+      slug: dto.slug,
+    };
+  }
+
+  // TODO: Define method to map from Entity to Dto
+}
