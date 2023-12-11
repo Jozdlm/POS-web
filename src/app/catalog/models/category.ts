cd70ce399f65639447b80b2c591b28230f3a4528
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-10 20:08:18 -0600

    Define los modelos category-dto y category

---
+++ src/app/catalog/models/category.ts
@@ -0,0 +1,13 @@
+export interface Category {
+  id?: number;
+  name: string;
+  description: string;
+  slug: string;
+}
+
+export interface CategoryDto {
+  id?: number;
+  name: string;
+  description: string;
+  slug: string;
+}
