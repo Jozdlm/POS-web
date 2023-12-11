6bc68cc6c5fdfa18fb8995d845ad558f398de81b
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-10 20:38:22 -0600

    Obtiene y renderiza las categorías en el template

--- src/app/catalog/pages/product-list/product-list.component.ts
+++ src/app/catalog/pages/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '@app/catalog/services/product.service';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CategoryService } from '@app/catalog/services/category.service';
 
 @Component({
   selector: 'app-product-list',
@@ -12,6 +13,7 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 })
 export class ProductListComponent {
   private readonly _productService = inject(ProductService);
+  public readonly categories$ = inject(CategoryService).getCategories();
   public readonly productCount$ = this._productService.getProductCount();
   public readonly products$ = this._productService.getProducts();
   public searchControl = new FormControl('');
