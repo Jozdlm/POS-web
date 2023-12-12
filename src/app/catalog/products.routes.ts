2548fca3daa8182d8ec78e1abd843e263183ca05
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-11 21:00:39 -0600

    Define la ruta add para el componente product-form

--- src/app/catalog/products.routes.ts
+++ src/app/catalog/products.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './pages/product-list/product-list.component';
+import { ProductFormComponent } from './pages/product-form/product-form.component';
 
 const PRODUCT_ROUTES: Routes = [
   { path: '', pathMatch: 'full', component: ProductListComponent },
+  { path: 'add', component: ProductFormComponent },
 ];
 
 export default PRODUCT_ROUTES;
