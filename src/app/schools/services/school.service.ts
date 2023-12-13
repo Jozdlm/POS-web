c12e197d3305f16ea7fd218d2f6346bc0ea240c9
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-12 22:20:12 -0600

    Obtiene los colegios desde la base de datos

--- src/app/schools/services/school.service.ts
+++ src/app/schools/services/school.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { SupabaseService } from '@app/core/services/supabase.service';
+import { Observable, from, map } from 'rxjs';
+import { School } from '../models/school';
+import { DbTables } from '@app/core/enums/db-tables';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SchoolService {
+  private readonly _db = inject(SupabaseService).supabase;
+
   constructor() {}
+
+  public getSchools(): Observable<School[]> {
+    return from(this._db.from(DbTables.SCHOOLS).select('*')).pipe(
+      map(({ data, error }) => {
+        if (error) throw new Error(error.message);
+
+        return data as School[];
+      }),
+    );
+  }
 }
