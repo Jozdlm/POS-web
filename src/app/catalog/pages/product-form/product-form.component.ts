b9c64da35ebc6146593add786423b300131d3ffa
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-16 11:23:50 -0600

    Navega hacia la ruta products al presionar cancelar

--- src/app/catalog/pages/product-form/product-form.component.ts
+++ src/app/catalog/pages/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryService } from '@app/catalog/services/category.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -10,5 +11,15 @@ import { CategoryService } from '@app/catalog/services/category.service';
   styleUrl: './product-form.component.scss'
 })
 export class ProductFormComponent {
+  private readonly _router = inject(Router);
   public readonly categories$ = inject(CategoryService).getCategories();
+
+  // TODO: Define the reactive form to a product
+
+  // TODO: Create a method that allows to create a product
+
+  // TODO: Create a method that cancel the operation and reset values
+  public cancelAndReset(): void {
+    this._router.navigateByUrl("products");
+  }
 }
