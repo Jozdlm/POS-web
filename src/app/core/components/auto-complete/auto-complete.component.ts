a0dbd71272f0f90e95e6338a134d4fe4aeb00dcb
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-17 10:07:15 -0600

    Define el valor por defecto que tendrá el form-control

--- src/app/core/components/auto-complete/auto-complete.component.ts
+++ src/app/core/components/auto-complete/auto-complete.component.ts
@@ -17,6 +17,7 @@ export class AutoCompleteComponent implements OnChanges {
 
   @Input({ required: true }) public listId: string = '';
   @Input({ required: true }) public placeholder: string = '';
+  @Input({ required: true }) public label: string = '';
   @Input({
     required: true,
     transform: (value: any[] | null) => {
@@ -33,6 +34,7 @@ export class AutoCompleteComponent implements OnChanges {
 
   ngOnChanges(): void {
     this.listState = this.options;
+    this.searchControl.setValue(this.placeholder);
   }
 
   public filterOptions(query: string): void {
