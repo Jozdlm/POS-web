0c9b67fe494f956b8e16a83408a587d595cafd22
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-18 09:55:00 -0600

    Obtiene los datos de la categoría mediante el id de la url

--- src/app/catalog/pages/category-form/category-form.component.ts
+++ src/app/catalog/pages/category-form/category-form.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CategoryService } from '@app/catalog/services/category.service';
+import { Category } from '@app/catalog/models/category';
 
 @Component({
   selector: 'app-category-form',
@@ -8,4 +12,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './category-form.component.html',
   styleUrl: './category-form.component.scss',
 })
-export class CategoryFormComponent {}
+export class CategoryFormComponent {
+  private readonly _route = inject(ActivatedRoute);
+  private readonly _categoryService = inject(CategoryService);
+  public category$: Observable<Category> | null = null;
+
+  constructor() {
+    this._route.paramMap.subscribe((params) => {
+      const categoryId = params.get('id');
+
+      if (categoryId) {
+        this.category$ = this._categoryService.getCategoryById(
+          parseInt(categoryId),
+        );
+      }
+    });
+  }
+}
