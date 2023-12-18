7bc9a1181cc9b61f25f56e33ae2196c44930c7f8
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-17 22:34:58 -0600

    Navega a la ruta correspondiente al dar click en el enlace

--- src/app/catalog/pages/category-list/category-list.component.ts
+++ src/app/catalog/pages/category-list/category-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryService } from '../../services/category.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-category-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.scss']
 })
