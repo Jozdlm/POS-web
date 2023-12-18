721cc4a66076c918e8d7eaaf7e1497da3e2f58ed
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-17 22:35:49 -0600

    Define las rutas para el componente category-form

--- src/app/catalog/categories.routes.ts
+++ src/app/catalog/categories.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { CategoryListComponent } from './pages/category-list/category-list.component';
+import { CategoryFormComponent } from './pages/category-form/category-form.component';
 
 const CATEGORIES_ROUTES: Routes = [
   { path: '', pathMatch: 'full', component: CategoryListComponent },
+  { path: 'add', component: CategoryFormComponent },
+  { path: 'edit/:id', component: CategoryFormComponent },
 ];
 
 export default CATEGORIES_ROUTES;
