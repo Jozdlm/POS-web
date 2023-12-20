b8ce13b28c3f18ef51dbbea4e51677249fb1125c
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-19 20:27:16 -0600

    genera el componente schools

---
+++ src/app/schools/pages/schools/schools.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-schools',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './schools.component.html',
+  styleUrl: './schools.component.scss',
+})
+export class SchoolsComponent {}
