74cd29dcc98d042cf845c5491845734ffe270d2a
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-19 22:31:01 -0600

    Genera el componente school-grades

---
+++ src/app/schools/pages/school-grades/school-grades.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-school-grades',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './school-grades.component.html',
+  styleUrl: './school-grades.component.scss',
+})
+export class SchoolGradesComponent {}
