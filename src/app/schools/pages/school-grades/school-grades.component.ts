89f83b24a222e90d1d2f37079d0cdf9d79c8f2ca
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-20 22:27:17 -0600

    Shows the schools grades at the school-grades component

--- src/app/schools/pages/school-grades/school-grades.component.ts
+++ src/app/schools/pages/school-grades/school-grades.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SchoolGradeService } from '@app/schools/services/school-grade.service';
 
 @Component({
   selector: 'app-school-grades',
@@ -8,4 +9,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './school-grades.component.html',
   styleUrl: './school-grades.component.scss',
 })
-export class SchoolGradesComponent {}
+export class SchoolGradesComponent {
+  public readonly schoolGrades$ = inject(SchoolGradeService).getSchoolGrades();
+}
