2c399445046ae895832bfe7391052129f118a5fb
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-21 19:06:35 -0600

    Defines the school-form routes

--- src/app/schools/schools.routes.ts
+++ src/app/schools/schools.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { SchoolsComponent } from './pages/schools/schools.component';
+import { SchoolFormComponent } from './pages/school-form/school-form.component';
 
 const SCHOOL_ROUTES: Routes = [
   { path: '', pathMatch: 'full', component: SchoolsComponent },
+  { path: 'add', component: SchoolFormComponent },
+  { path: 'edit/:id', component: SchoolFormComponent },
   { path: '**', redirectTo: '' },
 ];
 
