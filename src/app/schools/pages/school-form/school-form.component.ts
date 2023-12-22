b7bf4e9d0fd2d0210f07ecca5c14ecbfe9692903
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-21 18:56:44 -0600

    Generates the school-form component

---
+++ src/app/schools/pages/school-form/school-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-school-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './school-form.component.html',
+  styleUrl: './school-form.component.scss',
+})
+export class SchoolFormComponent {}
