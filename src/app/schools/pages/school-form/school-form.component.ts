eb57188e13942d6ecb90d987d14958a842b3dae0
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-21 20:27:51 -0600

    Shows dynamically the page title

--- src/app/schools/pages/school-form/school-form.component.ts
+++ src/app/schools/pages/school-form/school-form.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-school-form',
@@ -8,4 +9,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './school-form.component.html',
   styleUrl: './school-form.component.scss',
 })
-export class SchoolFormComponent {}
+export class SchoolFormComponent {
+  private readonly _activatedRoute = inject(ActivatedRoute);
+  public pageTitle: string = '';
+
+  constructor() {
+    this._activatedRoute.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      this.pageTitle = id ? 'Editar colegio' : 'Agregar colegio';
+    });
+  }
+}
