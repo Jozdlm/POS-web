9effc168a97489e5400b2a135e34080e731f12ef
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-21 20:51:34 -0600

    Links the form template to reactive form

--- src/app/schools/pages/school-form/school-form.component.ts
+++ src/app/schools/pages/school-form/school-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-school-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './school-form.component.html',
   styleUrl: './school-form.component.scss',
 })
@@ -13,10 +14,19 @@ export class SchoolFormComponent {
   private readonly _activatedRoute = inject(ActivatedRoute);
   public pageTitle: string = '';
 
+  public schoolForm = inject(FormBuilder).nonNullable.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    isActive: [true, [Validators.required]],
+  });
+
   constructor() {
     this._activatedRoute.paramMap.subscribe((params) => {
       const id = params.get('id');
       this.pageTitle = id ? 'Editar colegio' : 'Agregar colegio';
     });
+  }
+
+  public onSubmitForm(): void {
+    console.log('submited');
   }
 }
