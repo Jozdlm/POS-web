4242cf35316f50537742c6ff20276d2ab54de882
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-23 21:37:00 -0600

    Binds the product reactive form to the template form

--- src/app/catalog/pages/product-form/product-form.component.ts
+++ src/app/catalog/pages/product-form/product-form.component.ts
@@ -2,24 +2,36 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryService } from '@app/catalog/services/category.service';
 import { Router } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './product-form.component.html',
-  styleUrl: './product-form.component.scss'
+  styleUrl: './product-form.component.scss',
 })
 export class ProductFormComponent {
   private readonly _router = inject(Router);
   public readonly categories$ = inject(CategoryService).getCategories();
 
   // TODO: Define the reactive form to a product
+  public productForm = inject(FormBuilder).nonNullable.group({
+    name: [''],
+    barcode: [''],
+    sellingPrice: [0],
+    categoryId: [0],
+    inStock: [true],
+    isActive: [true],
+  });
 
   // TODO: Create a method that allows to create a product
+  public onSubmitForm(): void {
+    console.log('submitted');
+  }
 
   // TODO: Create a method that cancel the operation and reset values
   public resetAndReturn(): void {
-    this._router.navigateByUrl("products");
+    this._router.navigateByUrl('products');
   }
 }
