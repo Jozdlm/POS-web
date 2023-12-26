f2a821e76e66a63715912f1c33b362ce707ef642
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-26 13:06:21 -0600

    Uses the record status directive to show school status

--- src/app/schools/pages/schools/schools.component.ts
+++ src/app/schools/pages/schools/schools.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SchoolService } from '@app/schools/services/school.service';
 import { RouterModule } from '@angular/router';
+import { RecordStatusDirective } from '@app/common/directives/record-status.directive';
 
 @Component({
   selector: 'app-schools',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, RecordStatusDirective],
   templateUrl: './schools.component.html',
   styleUrl: './schools.component.scss',
 })
