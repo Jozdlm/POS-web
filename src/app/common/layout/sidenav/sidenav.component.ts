9372b3fe4fa2e0d5982dcf3eddd9c30f4ec2a23b
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-26 13:48:43 -0600

    Displays the nav item icon in to the template

--- src/app/common/layout/sidenav/sidenav.component.ts
+++ src/app/common/layout/sidenav/sidenav.component.ts
@@ -1,42 +1,49 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SessionService } from '@app/auth/services/session.service';
-import { NavItem } from '@app/common/interfaces/nav-item';
+import { NavItemWithIcon } from '@app/common/interfaces/nav-item';
 import { RouterModule } from '@angular/router';
+import { IconComponent } from '@app/common/components/icon.component';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, IconComponent],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
 })
 export class SidenavComponent {
   private readonly _sessionService = inject(SessionService);
-  public readonly navItems: NavItem[] = [
+  public readonly navItems: NavItemWithIcon[] = [
     {
       path: '',
       placeholder: 'Inicio',
+      icon: 'bi bi-house-door-fill',
     },
     {
       path: '/quotations',
       placeholder: 'Cotizaciones',
+      icon: 'bi bi-wallet-fill',
     },
     {
       path: '/products',
       placeholder: 'Productos',
+      icon: 'bi bi-bag-fill',
     },
     {
       path: '/categories',
       placeholder: 'Categorías',
+      icon: 'bi bi-tags-fill',
     },
     {
       path: '/schools',
       placeholder: 'Centros Educativos',
+      icon: 'bi bi-bank2',
     },
     {
       path: '/school-grades',
       placeholder: 'Grados Académicos',
+      icon: 'bi bi-bar-chart-fill',
     },
   ];
 
