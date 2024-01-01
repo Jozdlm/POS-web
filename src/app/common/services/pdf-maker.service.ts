259d0900a7c092c1d86d4055f68869aafae64a7c
Jozuan Lopez <jozuanlopez@gmail.com>
2023-12-31 23:33:49 -0600

    Compress the PDF file generated

--- src/app/common/services/pdf-maker.service.ts
+++ src/app/common/services/pdf-maker.service.ts
@@ -11,20 +11,28 @@ export class PdfMakerService {
     unit: 'mm',
     format: 'letter',
     putOnlyUsedFonts: true,
+    compress: true,
   };
 
-  constructor() {}
+  public readonly docWidth: number = 196;
+  public readonly topMargin: number = 10;
+  public readonly leftMargin: number = 10;
 
   public generatePDF(element: HTMLElement, docName: string): void {
     const maker = new jsPDF(this._pdfOptions);
-    const pdfWidth = 210;
 
     html2canvas(element, { scale: 3 }).then((canvas) => {
       const imgElement = canvas.toDataURL('image/png');
+      const pdfHeight = (canvas.height * this.docWidth) / canvas.width;
 
-      const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
-
-      maker.addImage(imgElement, 'PNG', 10, 10, pdfWidth, pdfHeight);
+      maker.addImage(
+        imgElement,
+        'PNG',
+        this.leftMargin,
+        this.topMargin,
+        this.docWidth,
+        pdfHeight,
+      );
       maker.html(element.innerHTML);
       maker.save(docName);
     });
