2e3a777c1ae813929b8b5986211c1541ae10e4e4
Jozuan Lopez <jozuanlopez@gmail.com>
2024-01-01 19:03:07 -0600

    Generate the quote header component

---
+++ src/app/quotations/pages/add-quotation/quote-header/quote-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-quote-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './quote-header.component.html',
+  styleUrl: './quote-header.component.scss',
+})
+export class QuoteHeaderComponent {}
