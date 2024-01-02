e20a37d29c89bd5df72798e4607644b8ff3d8f65
Jozuan Lopez <jozuanlopez@gmail.com>
2024-01-01 21:23:13 -0600

    Generate the quote-items component

---
+++ src/app/quotations/pages/add-quotation/quote-items/quote-items.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-quote-items',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './quote-items.component.html',
+  styleUrl: './quote-items.component.scss',
+})
+export class QuoteItemsComponent {}
