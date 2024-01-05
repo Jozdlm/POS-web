0dcad596ef144d479ea3318680e07cdf3a12cdad
Jozuan Lopez <jozuanlopez@gmail.com>
2024-01-04 21:27:39 -0600

    Generate quote confirmation component

---
+++ src/app/quotations/pages/add-quotation/quote-confirmation/quote-confirmation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-quote-confirmation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './quote-confirmation.component.html',
+  styleUrl: './quote-confirmation.component.scss',
+})
+export class QuoteConfirmationComponent {}
