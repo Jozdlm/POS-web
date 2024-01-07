58e6b67209475c81546b1dcd2bd735c55fce5aeb
Jozuan Lopez <jozuanlopez@gmail.com>
2024-01-06 18:04:49 -0600

    Create a quote when the button is clicked

--- src/app/quotations/pages/add-quotation/quote-confirmation/quote-confirmation.component.ts
+++ src/app/quotations/pages/add-quotation/quote-confirmation/quote-confirmation.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuotationStateService } from '@app/quotations/services/quotation-state.service';
+import { QuotationService } from '@app/quotations/services/quotation.service';
+import { QuoteMutation } from '@app/quotations/models/quotation';
 
 @Component({
   selector: 'app-quote-confirmation',
@@ -11,5 +13,21 @@ import { QuotationStateService } from '@app/quotations/services/quotation-state.
 })
 export class QuoteConfirmationComponent {
   private readonly _stateService = inject(QuotationStateService);
+  private readonly _quoteService = inject(QuotationService);
   public readonly quoteState = this._stateService.getStateSnapshot();
+
+  public onCreateQuote(): void {
+    const header: QuoteMutation = {
+      customerName: this.quoteState.customerName,
+      studentName: this.quoteState.studentName,
+      date: this.quoteState.date,
+      gradeId: this.quoteState.schoolGrade,
+      schoolId: this.quoteState.school,
+      promotionId: this.quoteState.promotionType,
+      totalAmmount: this.quoteState.total,
+    };
+    const items = this.quoteState.items;
+
+    this._quoteService.createQuotation({ ...header }, items);
+  }
 }
