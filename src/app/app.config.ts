3937b627431f5b02ff1e14e10b56a298e2e5d451
Jozuan Lopez <jozuanlopez@gmail.com>
2024-01-10 18:03:40 -0600

    Adds the hashLocationStrategy to the router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,13 @@ import {
 } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(BrowserModule),
-    provideRouter(APP_ROUTES),
+    provideRouter(APP_ROUTES, withHashLocation()),
     provideHttpClient(withInterceptorsFromDi()),
   ],
 };
