47f293d5735ef9e21adcffcb9d72df6f3fdb87d3
Jozuan Lopez <jozuanlopez@gmail.com>
2024-01-10 19:03:16 -0600

    Generates product-suggestions component

---
+++ src/app/quotations/pages/add-quotation/product-suggestions/product-suggestions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-suggestions',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-suggestions.component.html',
+  styleUrl: './product-suggestions.component.scss',
+})
+export class ProductSuggestionsComponent {}
