af1ade70004ab2f237e144a11385ad378f9fe04d
Jozuan Lopez <jozuanlopez@gmail.com>
2024-01-11 22:40:46 -0600

    Uses the api to access the supabase db

--- src/app/schools/services/school-grade.service.ts
+++ src/app/schools/services/school-grade.service.ts
@@ -3,17 +3,19 @@ import { SupabaseService } from 'src/app/common/services/supabase.service';
 import { SchoolGrade } from '../models/school-grades';
 import { DbTables } from '@app/common/enums/db-tables';
 import { Observable, from, map } from 'rxjs';
+import { ApiDbContext } from '@api/api-db-context.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SchoolGradeService {
   private readonly _db = inject(SupabaseService).supabase;
+  private readonly _dbContext = inject(ApiDbContext);
 
   constructor() {}
 
   public getSchoolGrades(): Observable<SchoolGrade[]> {
-    return from(this._db.from(DbTables.SCHOOL_GRADES).select('*')).pipe(
+    return this._dbContext.findAll<SchoolGrade>(DbTables.SCHOOL_GRADES).pipe(
       map(({ data, error }) => {
         if (error) throw new Error(error.message);
 
