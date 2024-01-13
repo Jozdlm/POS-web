c244c0195fcaf168750d5b9734326de64fa6a0f5
Jozuan Lopez <jozuanlopez@gmail.com>
2024-01-13 10:05:43 -0600

    Generates the quote-item component

---
+++ src/app/quotations/pages/add-quotation/molecules/quote-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-quote-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './quote-item.component.html',
+  styles: ``,
+})
+export class QuoteItemComponent {}
