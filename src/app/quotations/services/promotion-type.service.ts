72d19722745c5a8bccab71fa38ea440ccb04c24f
Jozuan Lopez <jozuanlopez@gmail.com>
2024-01-15 21:25:20 -0600

    Use the new api for retrieve data from the supabase

--- src/app/quotations/services/promotion-type.service.ts
+++ src/app/quotations/services/promotion-type.service.ts
@@ -1,23 +1,24 @@
 import { Injectable, inject } from '@angular/core';
-import { DbTables, SupabaseService } from '@app/common';
-import { Observable, from, map } from 'rxjs';
+import { DbTables } from '@app/common';
+import { Observable, map } from 'rxjs';
 import { PromotionType } from '../models/promotion-type';
+import { DbContext } from '@api/db-context.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PromotionTypeService {
-  private readonly _db = inject(SupabaseService).supabase;
+  private readonly _dbContext = inject(DbContext);
 
   public getPromotionById(promoId: number): Observable<PromotionType> {
-    return from(
-      this._db.from(DbTables.PROMOTION_TYPE).select().eq('id', promoId),
-    ).pipe(
-      map(({ data, error }) => {
-        if (error) throw new Error(error.message);
+    return this._dbContext
+      .find<PromotionType>(DbTables.PROMOTION_TYPE, 'id', promoId)
+      .pipe(
+        map(({ data, error }) => {
+          if (error) throw new Error(error.message);
 
-        return data[0] as PromotionType;
-      }),
-    );
+          return data[0] as PromotionType;
+        }),
+      );
   }
 }
