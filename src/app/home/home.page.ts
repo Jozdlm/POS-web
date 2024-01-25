af43dc7023636d7bbce155232995293dd11a6bcc
Jozuan Lopez <jozuanlopez@gmail.com>
2024-01-24 22:48:39 -0600

    Add a shortcut action button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, RouterModule],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
 })
