c2e302a9466375f5ed35a1911b7264f0d091eddb
Jozuan Lopez <jozuanlopez@gmail.com>
2024-01-28 20:04:20 -0600

    Refactors the item mutator code

--- src/app/quotations/item-mutator.helper.ts
+++ src/app/quotations/item-mutator.helper.ts
@@ -1,46 +1,42 @@
 import { QuotationItem } from './models/quotation-item';
 
+function getItemIndex(array: QuotationItem[], equalsTo: number): number {
+  const index = array.findIndex((el) => el.productId == equalsTo);
+
+  if (index === -1) {
+    throw new Error("The quote item doesn't exist on the provided array");
+  }
+
+  return index;
+}
+
 export function increaseItemQty(
   array: QuotationItem[],
   productId: number,
 ): QuotationItem[] {
-  const itemIndex = array.findIndex((el) => el.productId == productId);
-
-  if (itemIndex === -1) {
-    throw new Error("The quote item doesn't exist on the provided array");
-  }
-
+  const itemIndex = getItemIndex(array, productId);
   const currItem = array[itemIndex];
 
   currItem.quantity = currItem.quantity + 1;
   currItem.ammount = currItem.quantity * (currItem.price - currItem.discount);
 
-  const newArr = [...array];
-  newArr[itemIndex] = currItem;
-
-  return newArr;
+  array[itemIndex] = currItem;
+  return [...array];
 }
 
 export function decreaseItemQty(
   array: QuotationItem[],
   productId: number,
 ): QuotationItem[] {
-  const itemIndex = array.findIndex((el) => el.productId == productId);
-
-  if (itemIndex === -1) {
-    throw new Error("The quote item doesn't exist on the provided array");
-  }
-
+  const itemIndex = getItemIndex(array, productId);
   const currItem = array[itemIndex];
   const { quantity } = currItem;
 
   currItem.quantity = quantity > 1 ? quantity - 1 : quantity;
   currItem.ammount = currItem.quantity * (currItem.price - currItem.discount);
 
-  const newArr = [...array];
-  newArr[itemIndex] = currItem;
-
-  return newArr;
+  array[itemIndex] = currItem;
+  return [...array];
 }
 
 export function updateItemPrice(
@@ -48,19 +44,12 @@ export function updateItemPrice(
   productId: number,
   newPrice: number,
 ): QuotationItem[] {
-  const itemIndex = array.findIndex((el) => el.productId == productId);
-
-  if (itemIndex === -1) {
-    throw new Error("The quote item doesn't exist on the provided array");
-  }
-
+  const itemIndex = getItemIndex(array, productId);
   const currItem = array[itemIndex];
 
   currItem.price = newPrice > 0.01 ? newPrice : currItem.price;
   currItem.ammount = currItem.quantity * (currItem.price - currItem.discount);
 
-  const newArr = [...array];
-  newArr[itemIndex] = currItem;
-
-  return newArr;
+  array[itemIndex] = currItem;
+  return [...array];
 }
