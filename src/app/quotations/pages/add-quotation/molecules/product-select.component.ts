5a15ee3041a84224991ebcf159328003eda1e752
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-03 22:37:31 -0600

    Shows the search results in the template

--- src/app/quotations/pages/add-quotation/molecules/product-select.component.ts
+++ src/app/quotations/pages/add-quotation/molecules/product-select.component.ts
@@ -9,6 +9,7 @@ import {
   switchMap,
 } from 'rxjs';
 import { ProductService } from '@app/catalog/services/product.service';
+import { Product } from '@app/catalog/models/product';
 
 @Component({
   selector: 'app-product-select',
@@ -23,6 +24,28 @@ import { ProductService } from '@app/catalog/services/product.service';
         placeholder="Buscar producto"
         [formControl]="searchControl"
       />
+      <div>
+        <div class="list-group">
+          @for (item of results; track item.id) {
+            <div
+              class="list-group-item d-flex justify-content-between align-items-center"
+            >
+              <div class="ms-2 me-auto">
+                <div class="fw-medium">{{ item.name }}</div>
+                <div class="small-text">{{ item.barcode }}</div>
+              </div>
+              <div class="d-flex ms-3 column-gap-5 align-items-center">
+                <p class="mb-0">{{ item.sellingPrice | currency: 'GTQ' }}</p>
+                <div>
+                  <span class="badge bg-success rounded-pill">{{
+                    item.inStock ? 'En Stock' : 'Sin Stock'
+                  }}</span>
+                </div>
+              </div>
+            </div>
+          }
+        </div>
+      </div>
     </div>
   `,
   styles: `
@@ -35,20 +58,25 @@ import { ProductService } from '@app/catalog/services/product.service';
 export class ProductSelectComponent implements OnInit {
   private _productService = inject(ProductService);
   public searchControl = new FormControl<string>('');
+  public results: Product[] = [];
 
   ngOnInit(): void {
-    this.searchControl.valueChanges.pipe(
-      debounceTime(400),
-      distinctUntilChanged(),
-      filter((value) => typeof value === 'string'),
-      map((value) => value as string),
-      switchMap((value) => {
-        return this._productService.getProductsBy({
-          query: value,
-          field: 'name',
-          limit: 5,
-        });
-      }),
-    );
+    this.searchControl.valueChanges
+      .pipe(
+        debounceTime(400),
+        distinctUntilChanged(),
+        filter((value) => typeof value === 'string'),
+        map((value) => value as string),
+        switchMap((value) => {
+          return this._productService.getProductsBy({
+            query: value,
+            field: 'name',
+            limit: 5,
+          });
+        }),
+      )
+      .subscribe((values) => {
+        this.results = values;
+      });
   }
 }
