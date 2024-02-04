cb55554dd33b2e4af567e3f446774d0bea9997d6
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-04 08:01:41 -0600

    Deletes unused template and class methods

--- src/app/quotations/pages/add-quotation/tabs/quote-items.component.ts
+++ src/app/quotations/pages/add-quotation/tabs/quote-items.component.ts
@@ -1,17 +1,8 @@
-import { Component, DestroyRef, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  Subscription,
-  debounceTime,
-  distinctUntilChanged,
-  filter,
-  map,
-  switchMap,
-} from 'rxjs';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IconComponent } from '@app/common/components/icon.component';
 import { Product } from '@app/catalog/models/product';
-import { ProductService } from '@app/catalog/services/product.service';
 import { QuotationStateService } from '@app/quotations/services/quotation-state.service';
 import { RouterModule } from '@angular/router';
 import { ProductSelectComponent } from '../molecules/product-select.component';
@@ -34,40 +25,9 @@ import { ProductSelectComponent } from '../molecules/product-select.component';
   `,
 })
 export class QuoteItemsComponent {
-  private readonly _productService = inject(ProductService);
   private readonly _quoteStateService = inject(QuotationStateService);
-  private readonly _subscriptions = new Subscription();
   public quoteItems = this._quoteStateService.quoteItems;
   public subtotalQuote = this._quoteStateService.quoteSubtotal;
-  public searchControl = new FormControl('');
-  public filteredProducts: Product[] = [];
-
-  constructor() {
-    this.watchToQuerySearch();
-    inject(DestroyRef).onDestroy(() => this._subscriptions.unsubscribe());
-  }
-
-  private watchToQuerySearch(): void {
-    this._subscriptions.add(
-      this.searchControl.valueChanges
-        .pipe(
-          debounceTime(400),
-          distinctUntilChanged(),
-          filter((value) => typeof value === 'string'),
-          map((value) => value as string),
-          switchMap((value) => {
-            return this._productService.getProductsBy({
-              query: value,
-              field: 'name',
-              limit: 5,
-            });
-          }),
-        )
-        .subscribe((items) => {
-          this.filteredProducts = items;
-        }),
-    );
-  }
 
   public addItemToQuotation(item: Product): void {
     this._quoteStateService.addItem(item);
@@ -90,10 +50,5 @@ export class QuoteItemsComponent {
     const sellinPrice = parseFloat(inputValue);
 
     this._quoteStateService.updateSellingPrice(itemId, sellinPrice);
-  }
-
-  public clearSearchControl(): void {
-    this.searchControl.reset();
-    this.filteredProducts = [];
   }
 }
