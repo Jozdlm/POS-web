a3346924bd8d5e254d5af173ede635a6ad476dc0
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-03 22:28:16 -0600

    Gets the products when the usear is searching

--- src/app/quotations/pages/add-quotation/molecules/product-select.component.ts
+++ src/app/quotations/pages/add-quotation/molecules/product-select.component.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  switchMap,
+} from 'rxjs';
+import { ProductService } from '@app/catalog/services/product.service';
 
 @Component({
   selector: 'app-product-select',
@@ -24,6 +32,23 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
     }
   `,
 })
-export class ProductSelectComponent {
+export class ProductSelectComponent implements OnInit {
+  private _productService = inject(ProductService);
   public searchControl = new FormControl<string>('');
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      filter((value) => typeof value === 'string'),
+      map((value) => value as string),
+      switchMap((value) => {
+        return this._productService.getProductsBy({
+          query: value,
+          field: 'name',
+          limit: 5,
+        });
+      }),
+    );
+  }
 }
