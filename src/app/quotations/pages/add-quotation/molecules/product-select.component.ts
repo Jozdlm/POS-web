e1a4329b23cd0530df354b405170ed2a0f39d7b6
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-03 20:52:59 -0600

    Generates product select component

---
+++ src/app/quotations/pages/add-quotation/molecules/product-select.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-select',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <p>product-select works!</p> `,
+  styles: ``,
+})
+export class ProductSelectComponent {}
