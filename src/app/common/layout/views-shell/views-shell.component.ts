aa0c72f3bba0134eb8cd0e7dc53365f44027307f
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-06 22:39:36 -0600

    Create a button to switch the user view

--- src/app/common/layout/views-shell/views-shell.component.ts
+++ src/app/common/layout/views-shell/views-shell.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { SidenavComponent } from '../sidenav/sidenav.component';
+import { IconComponent } from '@app/common/components/icon.component';
 
 @Component({
   selector: 'app-views-shell',
   standalone: true,
-  imports: [CommonModule, RouterModule, NavbarComponent, SidenavComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NavbarComponent,
+    SidenavComponent,
+    IconComponent,
+  ],
   template: `
     <div class="app-wrapper">
       <app-sidenav class="border-end bg-light" />
@@ -16,6 +23,13 @@ import { SidenavComponent } from '../sidenav/sidenav.component';
         <div class="views-wrapper">
           <router-outlet></router-outlet>
         </div>
+      </div>
+
+      <div class="toggle-wrapper">
+        <button class="btn d-flex align-items-center toggle">
+          <span class="toggle-label"> Vista Admon. </span>
+          <ui-icon iconName="chevron-down" />
+        </button>
       </div>
     </div>
   `,
