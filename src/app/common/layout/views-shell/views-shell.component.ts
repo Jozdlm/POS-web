7feec591f3769262279a291f9788c265517a041d
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-06 22:17:01 -0600

    Moves the template to the component decorator

--- src/app/common/layout/views-shell/views-shell.component.ts
+++ src/app/common/layout/views-shell/views-shell.component.ts
@@ -8,7 +8,17 @@ import { SidenavComponent } from '../sidenav/sidenav.component';
   selector: 'app-views-shell',
   standalone: true,
   imports: [CommonModule, RouterModule, NavbarComponent, SidenavComponent],
-  templateUrl: './views-shell.component.html',
+  template: `
+    <div class="app-wrapper">
+      <app-sidenav class="border-end bg-light" />
+
+      <div class="content-wrapper">
+        <div class="views-wrapper">
+          <router-outlet></router-outlet>
+        </div>
+      </div>
+    </div>
+  `,
   styleUrl: './views-shell.component.scss',
 })
 export class ViewsShellComponent {}
