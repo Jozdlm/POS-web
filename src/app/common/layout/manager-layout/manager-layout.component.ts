4deda1ce959283f9d5141e988f15de1c329c6351
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-09 21:58:21 -0600

    Add styles to the manager layout

--- src/app/common/layout/manager-layout/manager-layout.component.ts
+++ src/app/common/layout/manager-layout/manager-layout.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidenavComponent } from '../sidenav/sidenav.component';
+import { RouterModule } from '@angular/router';
+import { IconComponent } from '@app/common/components/icon.component';
 
 @Component({
   selector: 'app-manager-layout',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <p>manager-layout works!</p> `,
+  imports: [CommonModule, SidenavComponent, RouterModule, IconComponent],
+  template: `
+    <div class="app-wrapper">
+      <app-sidenav class="border-end bg-light" />
+
+      <div class="content-wrapper">
+        <div class="views-wrapper">
+          <router-outlet></router-outlet>
+        </div>
+      </div>
+    </div>
+  `,
   styleUrl: './manager-layout.component.scss',
 })
 export class ManagerLayoutComponent {}
