29350db9128534db31c87fd42154917bd3acca37
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-09 20:35:34 -0600

    Generates the manager layout component

---
+++ src/app/common/layout/manager-layout/manager-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-manager-layout',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <p>manager-layout works!</p> `,
+  styleUrl: './manager-layout.component.scss',
+})
+export class ManagerLayoutComponent {}
