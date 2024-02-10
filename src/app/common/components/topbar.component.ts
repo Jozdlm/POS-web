7625c63c5b164e375001814b94f3ff4242205bd3
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-09 21:19:13 -0600

    Generates the component topbar

---
+++ src/app/common/components/topbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-topbar',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      topbar works!
+    </p>
+  `,
+  styles: ``
+})
+export class TopbarComponent {
+
+}
