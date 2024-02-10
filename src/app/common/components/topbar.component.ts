7275d029f0bb7f4105b2164d974551785a7160c4
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-09 21:43:58 -0600

    Adds styles to topbar component

--- src/app/common/components/topbar.component.ts
+++ src/app/common/components/topbar.component.ts
@@ -1,17 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IconComponent } from './icon.component';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IconComponent],
   template: `
-    <p>
-      topbar works!
-    </p>
-  `,
-  styles: ``
-})
-export class TopbarComponent {
+    <p class="fw-medium fs-6 mb-0">Librería La Joya</p>
 
-}
+    <div class="switch-view">
+      <button class="btn">
+        <ui-icon iconName="clipboard-data" />
+      </button>
+      <div class="switch-line"></div>
+      <button class="btn">
+        <ui-icon iconName="shop-window" />
+      </button>
+    </div>
+  `,
+  styles: `
+    :host {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 12px 32px;
+    }
+
+    .switch-view {
+      display: flex;
+      border: 1px solid #c2c2c2;
+      border-radius: 4px;
+    }
+
+    .switch-line {
+      min-height: 100%;
+      width: 1px;
+      background-color: #c2c2c2;
+    }
+
+    .switch-view > button:first-child {
+      border-radius: 4px 0 0 4px;
+    }
+
+    .switch-view > button:last-child {
+      border-radius: 0 4px 4px 0;
+    }
+  `,
+})
+export class TopbarComponent {}
