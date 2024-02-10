4351d7a848b1cae399e6c0c91b6594134ffc1315
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-09 21:41:16 -0600

    Implements the manager layout and their inner routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { anonClientGuard } from './auth/guards/anon-client.guard';
 import { loggedClientGuard } from './auth/guards/logged-client.guard';
 import { ViewsShellComponent } from './common/layout/views-shell/views-shell.component';
 import { PagePrefix } from './common/enums/pages';
+import { ManagerLayoutComponent } from './common/layout/manager-layout/manager-layout.component';
 
 // TODO: Set title property to each route
 export const APP_ROUTES: Routes = [
@@ -18,31 +19,38 @@ export const APP_ROUTES: Routes = [
     children: [
       {
         path: '',
-        pathMatch: 'full',
-        loadComponent: () => import('./home/home.page').then((c) => c.HomePage),
-      },
-      {
-        path: PagePrefix.QUOTATIONS,
-        loadChildren: () => import('./quotations/quotation.routes'),
-      },
-      {
-        path: PagePrefix.CATEGORIES,
-        loadChildren: () => import('./catalog/categories.routes'),
-      },
-      {
-        path: PagePrefix.PRODUCTS,
-        loadChildren: () => import('./catalog/products.routes'),
-      },
-      {
-        path: PagePrefix.SCHOOLS,
-        loadChildren: () => import('./schools/schools.routes'),
-      },
-      {
-        path: PagePrefix.GRADES,
-        loadComponent: () =>
-          import('./schools/pages/school-grades/school-grades.component').then(
-            (c) => c.SchoolGradesComponent,
-          ),
+        component: ManagerLayoutComponent,
+        children: [
+          {
+            path: '',
+            pathMatch: 'full',
+            loadComponent: () =>
+              import('./home/home.page').then((c) => c.HomePage),
+          },
+          {
+            path: PagePrefix.QUOTATIONS,
+            loadChildren: () => import('./quotations/quotation.routes'),
+          },
+          {
+            path: PagePrefix.CATEGORIES,
+            loadChildren: () => import('./catalog/categories.routes'),
+          },
+          {
+            path: PagePrefix.PRODUCTS,
+            loadChildren: () => import('./catalog/products.routes'),
+          },
+          {
+            path: PagePrefix.SCHOOLS,
+            loadChildren: () => import('./schools/schools.routes'),
+          },
+          {
+            path: PagePrefix.GRADES,
+            loadComponent: () =>
+              import(
+                './schools/pages/school-grades/school-grades.component'
+              ).then((c) => c.SchoolGradesComponent),
+          },
+        ],
       },
     ],
   },
