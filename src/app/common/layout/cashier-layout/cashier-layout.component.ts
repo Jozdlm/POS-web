7d4ed69208097ecbbfe9befd5db073c05529eb6b
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-10 20:05:41 -0600

    Generates the cashier layout component

---
+++ src/app/common/layout/cashier-layout/cashier-layout.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cashier-layout',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      cashier-layout works!
+    </p>
+  `,
+  styleUrl: './cashier-layout.component.scss'
+})
+export class CashierLayoutComponent {
+
+}
