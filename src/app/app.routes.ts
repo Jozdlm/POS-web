e6d4c7dbdac0883d8baaaa8a816f3e970778dc37
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-12 22:00:02 -0600

    Deletes unused component wich are splitted in two

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { anonClientGuard } from './auth/guards/anon-client.guard';
 import { loggedClientGuard } from './auth/guards/logged-client.guard';
-import { ViewsShellComponent } from './common/layout/views-shell/views-shell.component';
 import { PagePrefix } from './common/enums/pages';
 import { ManagerLayoutComponent } from './common/layout/manager-layout/manager-layout.component';
 import { CashierLayoutComponent } from './common/layout/cashier-layout/cashier-layout.component';
@@ -14,49 +13,42 @@ export const APP_ROUTES: Routes = [
     loadChildren: () => import('./auth/auth.routes'),
   },
   {
+    path: 'sell',
+    component: CashierLayoutComponent,
+  },
+  {
     path: '',
-    component: ViewsShellComponent,
+    component: ManagerLayoutComponent,
     canActivate: [loggedClientGuard],
     children: [
       {
         path: '',
-        component: ManagerLayoutComponent,
-        children: [
-          {
-            path: '',
-            pathMatch: 'full',
-            loadComponent: () =>
-              import('./home/home.page').then((c) => c.HomePage),
-          },
-          {
-            path: PagePrefix.QUOTATIONS,
-            loadChildren: () => import('./quotations/quotation.routes'),
-          },
-          {
-            path: PagePrefix.CATEGORIES,
-            loadChildren: () => import('./catalog/categories.routes'),
-          },
-          {
-            path: PagePrefix.PRODUCTS,
-            loadChildren: () => import('./catalog/products.routes'),
-          },
-          {
-            path: PagePrefix.SCHOOLS,
-            loadChildren: () => import('./schools/schools.routes'),
-          },
-          {
-            path: PagePrefix.GRADES,
-            loadComponent: () =>
-              import(
-                './schools/pages/school-grades/school-grades.component'
-              ).then((c) => c.SchoolGradesComponent),
-          },
-        ],
+        pathMatch: 'full',
+        loadComponent: () => import('./home/home.page').then((c) => c.HomePage),
       },
       {
-        path: 'sell',
-        component: CashierLayoutComponent,
-      }
+        path: PagePrefix.QUOTATIONS,
+        loadChildren: () => import('./quotations/quotation.routes'),
+      },
+      {
+        path: PagePrefix.CATEGORIES,
+        loadChildren: () => import('./catalog/categories.routes'),
+      },
+      {
+        path: PagePrefix.PRODUCTS,
+        loadChildren: () => import('./catalog/products.routes'),
+      },
+      {
+        path: PagePrefix.SCHOOLS,
+        loadChildren: () => import('./schools/schools.routes'),
+      },
+      {
+        path: PagePrefix.GRADES,
+        loadComponent: () =>
+          import('./schools/pages/school-grades/school-grades.component').then(
+            (c) => c.SchoolGradesComponent,
+          ),
+      },
     ],
   },
 ];
