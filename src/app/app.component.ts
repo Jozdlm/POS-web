815889c0dae0ce11036f2885d88541be84241116
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-12 22:02:28 -0600

    Deletes unused navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './common/layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   title = 'Librería La Joya - CMS';
