d1211d45931ec0b1c33b3854fa02d52df6fd37ea
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-22 21:38:17 -0600

    Move the topbar component to their own folder

--- src/app/common/components/topbar/topbar.component.ts
+++ src/app/common/components/topbar/topbar.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IconComponent } from './icon.component';
+import { IconComponent } from '../icon.component';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -9,10 +9,6 @@ import { MatIconModule } from '@angular/material/icon';
   standalone: true,
   imports: [CommonModule, IconComponent, RouterModule, MatIconModule],
   template: `
-    <button class="fw-medium fs-6 mb-0 btn" routerLink="/">
-      Librería La Joya
-    </button>
-
     <div class="switch-view">
       <button class="btn d-flex align-items-center column-gap-2 switch-button">
         <div class="d-flex align-items-center column-gap-2">
@@ -23,19 +19,6 @@ import { MatIconModule } from '@angular/material/icon';
       </button>
     </div>
   `,
-  styles: `
-    :host {
-      width: 100%;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      padding: 12px 32px;
-    }
-
-    .switch-button {
-      border: 1px solid #c2c2c2;
-      border-radius: 4px;
-    }
-  `,
+  styleUrl: `./topbar.component.scss`,
 })
 export class TopbarComponent {}
