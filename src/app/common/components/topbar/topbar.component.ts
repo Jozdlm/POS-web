84fc63f69e3b7e46575ef04ef6d020c2ed6e5ba8
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-25 21:55:53 -0600

    Add styles to the topbar elements

--- src/app/common/components/topbar/topbar.component.ts
+++ src/app/common/components/topbar/topbar.component.ts
@@ -10,26 +10,24 @@ import { TOP_BAR_LINKS } from './navigation-links';
   standalone: true,
   imports: [CommonModule, IconComponent, RouterModule, MatIconModule],
   template: `
-    <div class="navbar">
-      <div>
-        <div>
-          <div>IMG</div>
-          <p>Librería La Joya</p>
-        </div>
-        <div>
-          icon
+    <div class="topbar-wrapper">
+      <div class="navbar">
+        <button class="business">
+          <div class="business-info">
+            <div class="business-img"></div>
+            <p class="business-name">Librería La Joya</p>
+          </div>
+          <mat-icon class="business-icon">unfold_more</mat-icon>
+        </button>
+        <div class="navigation">
+          @for (link of navigationLinks; track $index) {
+            <a [routerLink]="link.path" class="navigation-link">{{
+              link.placeholder
+            }}</a>
+          }
         </div>
       </div>
-      <div class="navigation">
-        @for (link of navigationLinks; track $index) {
-          <a [routerLink]="link.path" class="navigation-link">{{
-            link.placeholder
-          }}</a>
-        }
-      </div>
-    </div>
-    <div>
-      Avatar
+      <div class="user-img"></div>
     </div>
   `,
   styleUrl: `./topbar.component.scss`,
