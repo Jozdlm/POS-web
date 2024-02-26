c88ba4bf09442c77a5db5733f50d46a9c823b181
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-25 22:03:55 -0600

    Remove unused import

--- src/app/common/components/topbar/topbar.component.ts
+++ src/app/common/components/topbar/topbar.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IconComponent } from '../icon.component';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { TOP_BAR_LINKS } from './navigation-links';
@@ -8,7 +7,7 @@ import { TOP_BAR_LINKS } from './navigation-links';
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [CommonModule, IconComponent, RouterModule, MatIconModule],
+  imports: [CommonModule, RouterModule, MatIconModule],
   template: `
     <div class="topbar-wrapper">
       <div class="navbar">
