bea4e79bdaba759441784822d2d3996db8353da7
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-26 08:19:36 -0600

    Changes the topbar navigation links

--- src/app/common/components/topbar/navigation-links.ts
+++ src/app/common/components/topbar/navigation-links.ts
@@ -2,8 +2,8 @@ import { NavItem } from '@app/common/interfaces/nav-item';
 
 export const TOP_BAR_LINKS: NavItem[] = [
   { path: 'overview', placeholder: 'Inicio' },
-  { path: 'cash-flow', placeholder: 'Flujo de Caja' },
   { path: 'sales', placeholder: 'Ventas' },
   { path: 'purchases', placeholder: 'Compras' },
-  { path: 'inventory', placeholder: 'Inventario' },
+  { path: 'products', placeholder: 'Productos' },
+  { path: 'analytics', placeholder: 'Estadísticas' },
 ];
