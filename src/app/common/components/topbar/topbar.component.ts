6bb66eced9e2b45a7f57a4fc5b5e6cd3ef2d3e3e
Jozuan Lopez <jozuanlopez@gmail.com>
2024-02-25 20:55:42 -0600

    Shows the navigation links in the topbar

--- src/app/common/components/topbar/topbar.component.ts
+++ src/app/common/components/topbar/topbar.component.ts
@@ -3,17 +3,25 @@ import { CommonModule } from '@angular/common';
 import { IconComponent } from '../icon.component';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { TOP_BAR_LINKS } from './navigation-links';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
   imports: [CommonModule, IconComponent, RouterModule, MatIconModule],
   template: `
-    <button class="switch-button">
-      <p>Administrador</p>
-      <mat-icon>expand_more</mat-icon>
-    </button>
+    <div>
+      <div></div>
+      <div class="navigation">
+        @for (link of navigationLinks; track $index) {
+          <a [routerLink]="link.path" class="navigation-link">{{ link.placeholder }}</a>
+        }
+      </div>
+    </div>
+    <div></div>
   `,
   styleUrl: `./topbar.component.scss`,
 })
-export class TopbarComponent {}
+export class TopbarComponent {
+  public readonly navigationLinks = TOP_BAR_LINKS;
+}
