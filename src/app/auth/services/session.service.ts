70074d672443c6b67cf0eb547b87ec728a95350c
Jozuan Lopez <jozuanlopez@gmail.com>
2024-03-03 20:28:36 -0600

    Deletes unused declarations in the service

--- src/app/auth/services/session.service.ts
+++ src/app/auth/services/session.service.ts
@@ -1,37 +1,16 @@
 import { Injectable, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { SupabaseService } from '@app/common/services/supabase.service';
 import { LoginCredentials } from '../models/login-credentials';
-import { BehaviorSubject, Observable, filter, map } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { AUTH } from '@api/index';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SessionService {
-  private readonly _supaService = inject(SupabaseService);
   private readonly _router = inject(Router);
-  private readonly _db = this._supaService.supabase;
-  private readonly _stateEmmitter = new BehaviorSubject<boolean | null>(null);
 
-  public isClientLogged$: Observable<boolean> = this._stateEmmitter
-    .asObservable()
-    .pipe(
-      filter((value) => typeof value === 'boolean'),
-      map((value) => value as boolean),
-    );
-
-  constructor() {
-    this._db.auth.onAuthStateChange((event, session) => {
-      if (event == 'SIGNED_OUT') {
-        this._stateEmmitter.next(false);
-      } else if (session) {
-        this._stateEmmitter.next(true);
-      }
-    });
-  }
-
-  public getCurrentSession(): Observable<string | null> {
+  public getSessionToken(): Observable<string | null> {
     return AUTH.getSessionToken();
   }
 
