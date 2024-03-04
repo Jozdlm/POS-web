f5281e7c3add6096e99bcb981313b2be9ebfffbb
Jozuan Lopez <jozuanlopez@gmail.com>
2024-03-03 21:53:39 -0600

    Rename the api controller name

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,12 +2,12 @@ import * as CategoryController from './categories.api';
 import * as PromotionTypeController from './promotion-type.api';
 import * as SchoolGradeController from './school-grades.api';
 import * as SessionController from './session.api';
-import * as EducationCenterController from './educational-center.api';
+import * as EducationalCenterController from './educational-center.api';
 
 export const API = {
   ...SessionController,
   ...CategoryController,
   ...PromotionTypeController,
   ...SchoolGradeController,
-  ...EducationCenterController,
+  ...EducationalCenterController,
 };
