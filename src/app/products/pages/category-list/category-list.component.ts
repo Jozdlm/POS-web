2c204ff04ce3003457e91772aaf1742b4907ca15
Jozuan Lopez <jozuanlopez@gmail.com>
2024-03-08 20:22:19 -0600

    Switch category-list component to inline template

--- src/app/products/pages/category-list/category-list.component.ts
+++ src/app/products/pages/category-list/category-list.component.ts
@@ -7,10 +7,37 @@ import { MatDialog } from '@angular/material/dialog';
 import { CategoryFormComponent } from '../category-form/category-form.component';
 
 @Component({
-  selector: 'app-category-list',
   standalone: true,
   imports: [CommonModule, RouterModule, RecordStatusDirective],
-  templateUrl: './category-list.component.html',
+  template: `
+    <div class="wrapper">
+      <div class="d-flex justify-content-between align-items-center mb-3">
+        <h1 class="fs-3">Categorías</h1>
+        <a class="btn btn-primary" routerLink="add">Nueva Categoría</a>
+        <button (click)="openDialog()">Open Dialog</button>
+      </div>
+
+      <div class="list-group">
+        @for (item of categories$ | async; track item.id) {
+          <div
+            class="list-group-item d-flex justify-content-between align-items-center"
+          >
+            <div>
+              <p class="fw-medium fs-5 mb-0">
+                {{ item.name }}
+              </p>
+              <p class="mb-2">{{ item.description }}</p>
+              <span class="badge rounded-pill" [recordStatus]="item.isActive">
+              </span>
+            </div>
+            <div>
+              <a class="btn" [routerLink]="['edit', item.id]">Editar</a>
+            </div>
+          </div>
+        }
+      </div>
+    </div>
+  `,
   styleUrls: ['./category-list.component.scss'],
 })
 export class CategoryListComponent {
