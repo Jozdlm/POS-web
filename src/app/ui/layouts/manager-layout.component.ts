512a7815becbfa2cc0987c3349f8f2cce209a9a3
Jozuan Lopez <jozuanlopez@gmail.com>
2024-03-24 00:26:33 -0600

    Get rid of scss component file

--- src/app/ui/layouts/manager-layout.component.ts
+++ src/app/ui/layouts/manager-layout.component.ts
@@ -17,10 +17,9 @@ import { TopbarComponent } from '@app/ui/components/topbar/topbar.component';
   template: `
     <app-topbar />
 
-    <div class="mx-auto w-full max-w-screen-2xl">
+    <div class="mx-auto w-full max-w-screen-2xl px-4 py-6">
       <router-outlet></router-outlet>
     </div>
   `,
-  styleUrl: './manager-layout.component.scss',
 })
 export class ManagerLayoutComponent {}
