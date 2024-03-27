4834e4f4c203a837fc0359a738431516da1cba18
Jozuan Lopez <jozuanlopez@gmail.com>
2024-03-26 20:46:50 -0600

    Fix the manager layout grid

--- src/app/ui/layouts/manager-layout.component.ts
+++ src/app/ui/layouts/manager-layout.component.ts
@@ -17,11 +17,13 @@ import { TOP_BAR_LINKS } from '../components/topbar/navigation-links';
   ],
   template: `
     <div
-      class="grid w-full max-w-[100vw] grid-cols-[280px_minmax(max-content,_1fr)]"
+      class="grid w-full h-full max-w-[100vw] grid-cols-[280px_minmax(max-content,_1fr)]"
     >
       <app-sidenav [navigationItems]="navItems" />
-      <div class="mx-auto w-full max-w-screen-lg px-4 py-2">
-        <router-outlet></router-outlet>
+      <div class="max-h-screen w-full overflow-auto p-4">
+        <div class="mx-auto max-w-screen-md">
+          <router-outlet></router-outlet>
+        </div>
       </div>
     </div>
   `,
