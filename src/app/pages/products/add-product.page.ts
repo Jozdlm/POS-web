cfb01646a451f9d691ffce11e09dbaef9c2e5825
Jozuan Lopez <jozuanlopez@gmail.com>
2024-03-28 21:57:50 -0600

    Replace bootstrap class names by tailwind class names

--- src/app/pages/products/add-product.page.ts
+++ src/app/pages/products/add-product.page.ts
@@ -6,53 +6,77 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductService } from '@app/features/products/product.service';
 import { Subscription, of, switchMap } from 'rxjs';
 import { Product } from '@app/features/products/product';
+import { InputFieldDirective } from '@app/ui';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, InputFieldDirective],
   template: `
-    <div class="mx-auto w-full max-w-[480px]">
-      <h1 class="fs-4 mb-4">{{ pageTitle }}</h1>
+    <div
+      class="mx-auto w-full max-w-max rounded-lg border border-slate-300 px-6 py-4"
+    >
+      <h1 class="text-base mb-4 font-medium text-slate-900">{{ pageTitle }}</h1>
       <form
+        class="border-t border-t-slate-300 pt-6"
         autocomplete="off"
         [formGroup]="productForm"
         (ngSubmit)="onSubmitForm()"
       >
-        <div class="mb-3">
-          <label for="productName" class="form-label">Producto</label>
+        <div class="mb-4">
+          <label
+            for="productName"
+            class="inline-block w-40 font-medium text-gray-700"
+            >Producto</label
+          >
           <input
+            uiInputField
             type="text"
-            class="form-control"
+            class="w-80"
             id="productName"
             formControlName="name"
           />
         </div>
-        <div class="mb-3">
-          <label for="barcode" class="form-label">Código Barras</label>
+        <div class="mb-4">
+          <label
+            for="barcode"
+            class="inline-block w-40 font-medium text-gray-700"
+            >Código Barras</label
+          >
           <input
+            uiInputField
             type="text"
-            class="form-control"
+            class="w-80"
             id="barcode"
             formControlName="barcode"
           />
         </div>
-        <div class="mb-3">
-          <label for="sellingPrice" class="form-label">Precio Venta</label>
+        <div class="mb-4">
+          <label
+            for="sellingPrice"
+            class="inline-block w-40 font-medium text-gray-700"
+            >Precio Venta</label
+          >
           <input
+            uiInputField
             type="number"
             step="0.01"
             min="0.00"
-            class="form-control"
+            class="w-80"
             id="sellingPrice"
             formControlName="sellingPrice"
           />
         </div>
-        <div class="mb-3">
-          <label for="category" class="form-label">Categoría</label>
+        <div class="mb-4">
+          <label
+            for="category"
+            class="inline-block w-40 font-medium text-gray-700"
+            >Categoría</label
+          >
           <select
-            class="form-select"
+            class="w-80"
             id="category"
             formControlName="categoryId"
+            uiInputField
           >
             <option value="0">Escoge una categoría</option>
             @for (item of categories$ | async; track item.id) {
@@ -60,37 +84,54 @@ import { Product } from '@app/features/products/product';
             }
           </select>
         </div>
-        <div class="mb-3">
-          <label for="stockStatus" class="form-label">Inventario</label>
+        <div class="mb-4">
+          <label
+            for="stockStatus"
+            class="inline-block w-40 font-medium text-gray-700"
+            >Inventario</label
+          >
           <select
-            class="form-select"
+            class="w-80"
             id="stockStatus"
             formControlName="inStock"
+            uiInputField
           >
             <option value="false">Sin existencias</option>
             <option value="true" selected>Con Existencias</option>
           </select>
         </div>
-        <div class="mb-3">
-          <label for="productStatus" class="form-label">Estado</label>
+        <div>
+          <label
+            for="productStatus"
+            class="inline-block w-40 font-medium text-gray-700"
+            >Estado</label
+          >
           <select
-            class="form-select"
+            class="w-80"
             id="productStatus"
             formControlName="isActive"
+            uiInputField
           >
             <option value="false">Desactivado</option>
             <option value="true" selected>Activo</option>
           </select>
         </div>
-        <div class="d-flex column-gap-2">
+        <div
+          class="mt-6 flex justify-end gap-x-3 border-t border-t-slate-300 pt-4"
+        >
           <button
             type="button"
-            class="btn btn-outline-secondary"
+            class="rounded-md border border-slate-300 px-3 py-2"
             (click)="resetAndReturn()"
           >
             Cancelar
           </button>
-          <button type="submit" class="btn btn-primary">Guardar cambios</button>
+          <button
+            type="submit"
+            class="rounded-md bg-slate-700 px-3 py-2 text-white"
+          >
+            Guardar cambios
+          </button>
         </div>
       </form>
     </div>
@@ -103,7 +144,7 @@ export class AddProductPage {
   private readonly _subscriptions = new Subscription();
   public readonly categories$ = inject(CategoryService).getCategories();
   public productId: number | null = null;
-  public pageTitle: string = 'Crear producto';
+  public pageTitle: string = 'Agregar nuevo producto';
 
   public productForm = inject(FormBuilder).nonNullable.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
