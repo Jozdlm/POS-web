ac0e0f76d4167229dcbd1ef60a89ca6a48409311
Jozuan Lopez <jozuanlopez@gmail.com>
2024-05-29 08:32:39 -0600

    Adds new links to navigation links array

--- src/app/common/constants/navigation-links.ts
+++ src/app/common/constants/navigation-links.ts
@@ -2,8 +2,10 @@ import { NavItem } from '@app/common/interfaces/nav-item';
 
 export const NAVIGATION_LINKS: NavItem[] = [
   { path: 'overview', placeholder: 'Inicio' },
-  { path: 'transactions', placeholder: 'Movimientos' },
+  { path: 'pos', placeholder: 'POS' },
+  { path: 'sales', placeholder: 'Ventas' },
   { path: 'quotations', placeholder: 'Cotizaciones' },
-  { path: 'products', placeholder: 'Productos' },
+  { path: 'purchases', placeholder: 'Compras' },
+  { path: 'inventory', placeholder: 'Inventario' },
   { path: 'analytics', placeholder: 'Estadísticas' },
 ];
